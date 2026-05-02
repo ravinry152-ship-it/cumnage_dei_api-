c67ec2655a3e58e2b7e13230e9593766f16b2fb5
yravin <ryravin25@Email.com>
2026-05-02 14:29:19 +0700

    update code

--- title_system/mytitle/urls.py
+++ title_system/mytitle/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from. import views
-
+from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('api/register/', views.Register.as_view(), name='register'),
     path('api/login/', views.Login.as_view(), name='login'),
@@ -8,7 +8,8 @@ urlpatterns = [
     path('api/setup-system/', views.SetupSystem.as_view(), name='setup_system'),
     path('api/detail_setup_system/<int:pk>/', views.SetupSystemDetail.as_view(), name='detail_setup_system'),
     path('api/userdata/', views.UserData.as_view(), name = 'userdata'),
-    path('api/detail_userdata/<int:pk>/', views.UserDataDetail.as_view(), name = 'detail_userdata')
+    path('api/detail_userdata/<int:pk>/', views.UserDataDetail.as_view(), name = 'detail_userdata'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 
 
 ]
